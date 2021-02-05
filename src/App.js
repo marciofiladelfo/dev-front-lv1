f5e81a3ac29f697dc7f026b427310b7d797125cb
marciofiladelfo <marcio.kabana@gmail.com>
2021-02-04 21:33:18 -0300

    Solucionado bug e revisado

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import Header from "./components/Menu";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-// cadastro de app no GitHub para não exceder número de requisições
-const securing = {
-  baseUrl: "https://api.github.com",
-  client_id: "Iv1.7e879e2f04f15fc8",
-  client_secret: "SHA256:izvwPuwh5WW6gWvJcykfcwNpfxNEQOP7Xk4qLyGZiiQ",
-};
+
+const baseUrl = "https://api.github.com";
+
 
 function App() {
   let [membersAPI, setMembersAPI] = useState([]);
@@ -18,20 +15,20 @@ function App() {
   const getMembers = () => {
     axios
       .get(
-        `${securing.baseUrl}/orgs/aws/members?client_id=${securing.client_id}&client_secret=${securing.client_secret}`
+        `${baseUrl}/orgs/aws/members`
       )
       .then((res) => {
         setMembersAPI(res.data);
       });
     console.log(membersAPI)
-    let userName = membersAPI.map((e) => {
+     let userName = membersAPI.map((e) => {
       return e.login;
     });
     if(membersAPI){
       userName.forEach((e) => {
         axios
           .get(
-            `${securing.baseUrl}/users/${e}?client_id=${securing.client_id}&client_secret=${securing.client_secret}`
+            `${baseUrl}/users/${e}`
           )
           .then((response) => {
             setUsersAPI((state) => [...state, response.data]);
